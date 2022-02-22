91911295f011aac332682635994847080e635052
jonahseif <72671007+jonahseif@users.noreply.github.com>
2022-02-22 12:20:37 -0800

    completed assignment 4

--- R/autopower.R
+++ R/autopower.R
@@ -1,17 +1,18 @@
-#' Automobile Fuel Efficiency
-#' 
-#' This function determines automobile fuel efficiency by calculating the power required to keep a car moving at a given speed
-#' @param V vehicle speed (m/s)
+
+#' Power Required by Speed
+#'
+#' This function determines the power required to keep a vehicle moving at 
+#' a given speed
+#' @param cdrag coefficient due to drag default=0.3 
+#' @param crolling coefficient due to rolling/friction default=0.015
+#' @param v vehicle speed (m/2)
 #' @param m vehicle mass (kg)
-#' @param A vehicle surface area (m2)
-#' @param g acceleration due to gravity (m/s2) default=9.8
-#' @param p_air density of air (kg/m3) default=1.2
-#' @param crolling rolling coefficient default=0.015
-#' @param c_drag aerodynamic resistive coefficient default=0.3
+#' @param A area of front of vehicle (m2)
+#' @param g acceleration due to gravity (m/s) default=9.8
+#' @param pair (kg/m3) default =1.2
 #' @return power (W)
-#' 
-# function definition 
-vehicle_power = function(speed, mass, area, g=9.8, p_air=1.2, crolling=0.015, c_drag=0.3) {
-result = crolling * mass * g * speed + 1/2 * area * p_air * c_drag * speed^3
-return(result)
+
+autopower = function(V, m, A, cdrag=0.3, crolling=0.015,pair=1.2,g=9.8) {
+  P = crolling*m*g*V + 1/2*A*pair*cdrag*V**3
+  return(P)
 }
